224a1853104c8b64979a9b9257edf214c1d8459a
Ziad Zebib <zebibziad@gmail.com>
2021-01-01 02:58:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,6 +9,7 @@ def nyc_pigeon_organizer(data)
         end
         if !final_array[name][key]
           !final_array[name][key] = []
-    final_array
-  b
+        end
+        final_array[name][key].push(inner_key)
+  
 end
