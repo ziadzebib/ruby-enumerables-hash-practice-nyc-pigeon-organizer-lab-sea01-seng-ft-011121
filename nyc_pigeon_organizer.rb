445e46688ea9df623db2ec9872df1839ef61b706
Ziad Zebib <zebibziad@gmail.com>
2021-01-01 02:56:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    fina
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+    end
+    if !final_array[name][key]
+      
+    final_array
   binding.pry
 end
